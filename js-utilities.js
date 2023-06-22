3fb99c5c27ed73a2c8b930d01b3f1def622027b7
Cameron <ctharp1988@gmail.com>
2023-06-22 15:18:31 -0500

    added functions

--- js-utilities.js
+++ js-utilities.js
@@ -23,21 +23,80 @@ class Utilities {
     };
   };
 
-  static once = () => {};
+  static memoize = (func) => {
+    const cache = new Map();
 
-  static curry = () => {};
+    return function() {
+      const key = JSON.stringify(arguments);
+      if (cache.has(key)) {
+        return cache.get(key);
+      }
+      const result = func.apply(this, arguments);
+      cache.set(key, result);
+      return result;
+    };
+  };
 
-  static partial = () => {};
+  static curry = (func, arity = func.length) => {
+    return function curried(...args) {
+      if (args.length >= arity) return func(...args);
+      return function(...moreArgs) {
+        return curried(...args, ...moreArgs);
+      };
+    };
+  };
 
-  static pipe = () => {};
+  static partial = (func, ...args) => {
+    return function partiallyApplies(...moreArgs) {
+      return func(...args, ...moreArgs);
+    };
+  };
 
-  static compose = () => {};
+  static pipe = (...funcs) => {
+    return function piped(...args) {
+      return funcs.reduce((result, func) => [func.call(this, ...result)], args)[0];
+    }
+  };
 
-  static pick = () => {};
+  static compose = (...funcs) => {
+    return function composed(...args) {
+      return funcs.reduceRight((result, func) => [func.call(this, ...result)], args)[0];
+    }
+  };
 
-  static omit = () => {};
+  static pick = (obj, keys) => {
+    return keys.reduce((acc, key) => {
+      if (obj.hasOwnProperty(key)) {
+        acc[key] = obj[key];
+      }
+      return acc;
+    }, {})
+  };
 
-  static zip = () => {};
+  static omit = (obj, keys) => {
+    return Object.keys(obj)
+      .filter(key => !keys.includes(key))
+      .reduce((acc, key) => {
+        acc[key] = obj[key];
+        return acc;
+      }, {})
+  };
+
+  static zip = (...arrays) => {
+    const maxLength = Math.max(...arrays.map(array => array.length));
+    return Array.from({ length: maxLength }).map((_, i) => {
+      return Array.from({ length: arrays.length }, (_, j) => array[j][i]);
+    });
+  };
+
+  static chunk = (array, chunkSize) => {
+    const chunks = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+      const chunk = array.slice(i, i + chunkSize);
+      chunks.push(chunk);
+    }
+    return chunks;
+  }
 }
 
 export default Utilities;
